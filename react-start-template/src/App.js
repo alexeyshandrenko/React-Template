52c1eb81e8b3c88bec33c49a5bdefd282b0765eb
alexeyshandrenko <adshandrenko@gmail.com>
2024-02-19 20:54:31 +0300

    rewriting to createElement

--- react-start-template/src/App.js
+++ react-start-template/src/App.js
@@ -1,30 +1,36 @@
+import React, { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// В App.js представлена функция (компонент), которая возвращает JSX-код.
-// Это пример декларативного стиля написания кода,
-// так как мы не пишем последовательность команд для того, чтобы отобразить нужный результат,
-// а сразу напрямую его запрашиваем
+// Здесь представлен императивный подход, так как мы вызываем конкретную инструкцию методов, для возврата react-элементов
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				{},
+				'Edit ',
+				createElement('code', {}, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', {}, new Date().getFullYear()),
+		),
 	);
 }
 
